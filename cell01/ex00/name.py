6fca04fbce2f1b1a62ce0c999d1c1e7658669234
66070271 <66070271@kmitl.ac.th>
2025-09-15 22:29:14 +0700

    commit cell1

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Thitipat"
+last_name = "Sonprasertkul"
+print(first_name,last_name) 
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Thitipat"
+last_name = "Sonprasertkul"
+whole_name = first_name + " " + last_name
+print(whole_name)
