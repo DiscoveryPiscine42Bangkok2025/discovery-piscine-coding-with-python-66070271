07b87cae01c44840f372834150e2fdf7aacda2fd
66070271 <66070271@kmitl.ac.th>
2025-09-17 20:55:00 +0700

    com5

---
+++ cell05/ex01/play_with_array.py
@@ -0,0 +1,6 @@
+og = [2,8,9,48,8,22,-12,2]
+new = []
+for i in og:
+    new.append(i+2)
+print(f"Original array : {og}")
+print(f"New array : {new}")
---
+++ cell05/ex03/play_with_array.py
@@ -0,0 +1,7 @@
+og = [2,8,9,48,8,22,-12,2]
+new = []
+for i in og:
+    if(i>5):
+        new.append(i+2)
+print(f"Original array : {og}")
+print(f"New array : {set(new)}")
